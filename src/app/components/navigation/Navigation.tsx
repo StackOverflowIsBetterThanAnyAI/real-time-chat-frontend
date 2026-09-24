56d1bbc2c556057d9ae165475b54fbc4f3766732
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-09-24 16:44:40 +0200

    h1

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -2,7 +2,7 @@ const Navigation = () => {
     return (
         <nav className="w-full bg-zinc-600 flex items-center justify-between h-16 px-2 sm:px-4 py-1 md:py-2">
             <div className="flex justify-center items-center gap-2 md:gap-4">
-                <h1>Dieter-Chat</h1>
+                <h1 className="text-large">Dieter-Chat</h1>
             </div>
         </nav>
     )
