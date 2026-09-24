5c576e7aa35166d2b342d470ead98b371ea07a76
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-09-24 18:29:03 +0200

    profile picture

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -1,10 +1,64 @@
+'use client'
+
+import Image from 'next/image'
+import { useContext, useState } from 'react'
+import profile_picure from '@/assets/profile_picture.jpg'
+import { ContextIsLoggedIn } from '@/context/ContexIsLoggedIn'
+import { getStoredSessionData } from '@/utils/getStoredSessionData'
+import { setItemInSessionStorage } from '@/utils/setItemInSessionStorage'
+
 const Navigation = () => {
+    const parsedSessionData = getStoredSessionData()
+
+    const contextIsLoggedIn = useContext(ContextIsLoggedIn)
+    if (!contextIsLoggedIn) {
+        throw new Error(
+            'Navigation must be used within a ContextIsLoggedIn.Provider'
+        )
+    }
+    const [isLoggedIn, setIsLoggedIn] = contextIsLoggedIn
+
+    const [isSettingsExpanded, setIsSettingsExpanded] = useState<boolean>(
+        () => {
+            const data = parsedSessionData?.issettingsexpanded
+            if (data && typeof data === 'boolean') {
+                return data
+            }
+            setItemInSessionStorage('issettingsexpanded', false)
+            return false
+        }
+    )
+
+    const handleClick = () => {
+        setIsSettingsExpanded(() => {
+            const nextVal = !isSettingsExpanded
+            setItemInSessionStorage('issettingsexpanded', nextVal)
+            return nextVal
+        })
+    }
+
     return (
-        <nav className="w-full bg-zinc-600 flex items-center justify-between h-16 px-2 sm:px-4 py-1 md:py-2">
-            <div className="flex justify-center items-center gap-2 md:gap-4">
-                <h1 className="text-large">Dieter-Chat</h1>
-            </div>
-        </nav>
+        <>
+            <nav className="w-full bg-zinc-600 flex items-center justify-between h-16 px-2 sm:px-4 py-1 md:py-2">
+                <div className="flex justify-between items-center gap-2 md:gap-4 w-full">
+                    <h1 className="text-large">Dieter-Chat</h1>
+                    {isLoggedIn && (
+                        <button onClick={handleClick}>
+                            <Image
+                                alt="profile picture"
+                                src={profile_picure}
+                                className="w-12 h-12 rounded-full outline-2 outline-zinc-100"
+                            />
+                        </button>
+                    )}
+                </div>
+            </nav>
+            {isSettingsExpanded && isLoggedIn && (
+                <aside className="absolute w-96 top-16 right-0 bottom-4 bg-zinc-700 p-2 border-2 border-zinc-800 rounded-b-xl">
+                    Hello World!
+                </aside>
+            )}
+        </>
     )
 }
 
