ee15dd3a8ac24f99184e66b4233d344b10f7f5a9
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-09-25 13:34:54 +0200

    @ import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '@/app/globals.css'
 import { Providers } from '@/app/providers'
 import Footer from '@/app/components/footer/Footer'
 import Navigation from '@/app/components/navigation/Navigation'
